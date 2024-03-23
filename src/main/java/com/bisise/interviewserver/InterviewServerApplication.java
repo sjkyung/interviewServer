6a2c005f06b8186b485c6e8a42266cc6539b27ed
sjkyomg <sjkyomg@naver.com>
2024-03-23 16:59:49 +0900

    Initial commit

---
+++ src/main/java/com/bisise/interviewserver/InterviewServerApplication.java
@@ -0,0 +1,13 @@
+package com.bisise.interviewserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewServerApplication.class, args);
+    }
+
+}
