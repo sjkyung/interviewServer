d0ebfc2874b98eba41553a660c4b31acc7992bd7
sjkyomg <sjkyomg@naver.com>
2024-03-25 20:07:25 +0900

    yml 수정

--- src/test/java/com/bisise/interviewserver/InterviewServerApplicationTests.java
+++ src/test/java/com/bisise/interviewserver/InterviewServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InterviewServerApplicationTests {
 
-    @Test
+    /*@Test
     void contextLoads() {
-    }
+    }*/
 
 }
